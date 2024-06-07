3cb304e83d480fc29ba0ac99139a156f34b148cc
subinsong01 <haely97@naver.com>
2024-06-07 16:01:56 +0900

    showMyPageMessage & showCustomerServiceMessage 팝업 띄움

--- app/src/main/java/com/shushub/finalproject/MainActivity.java
+++ app/src/main/java/com/shushub/finalproject/MainActivity.java
@@ -2,6 +2,9 @@ package com.shushub.finalproject;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,5 +26,13 @@ public class MainActivity extends AppCompatActivity {
         }
         isCover01 = !isCover01;
     }
+    public void showCustomerServiceMessage(View view) {
+        // 고객센터 버튼 클릭 시 토스트 메시지 표시
+        Toast.makeText(this, "고객센터버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
+    }
 
-}
+    public void showMyPageMessage(View view) {
+        // 마이페이지 버튼 클릭 시 토스트 메시지 표시
+        Toast.makeText(this, "마이페이지버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
+    }
+    }
