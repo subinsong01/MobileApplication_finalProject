329d8aff2d0b50c1f23e48b04751dc074f8f5f09
subinsong01 <haely97@naver.com>
2024-06-08 19:07:25 +0900

    login 팝업 설정

--- app/src/main/java/com/shushub/finalproject/MainActivity.java
+++ app/src/main/java/com/shushub/finalproject/MainActivity.java
@@ -1,8 +1,13 @@
 package com.shushub.finalproject;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView loginImage = findViewById(R.id.login_btn);
+        loginImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showLoginDialog();
+            }
+        });
     }
 
     public void changeCover(View view) { //cover 버튼 누르면 색상 변경
@@ -26,6 +39,7 @@ public class MainActivity extends AppCompatActivity {
         }
         isCover01 = !isCover01;
     }
+
     public void showCustomerServiceMessage(View view) {
         // 고객센터 버튼 클릭 시 토스트 메시지 표시
         Toast.makeText(this, "고객센터버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
@@ -35,4 +49,43 @@ public class MainActivity extends AppCompatActivity {
         // 마이페이지 버튼 클릭 시 토스트 메시지 표시
         Toast.makeText(this, "마이페이지버튼이 클릭되었습니다", Toast.LENGTH_SHORT).show();
     }
+
+    private void showLoginDialog() {
+        // 커스텀 레이아웃 인플레이트
+        LayoutInflater inflater = getLayoutInflater();
+        View dialogView = inflater.inflate(R.layout.activity_login, null);
+
+        // AlertDialog 생성
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setView(dialogView);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
+        // 버튼 및 EditText 참조
+        EditText usernameEditText = dialogView.findViewById(R.id.username);
+        EditText passwordEditText = dialogView.findViewById(R.id.password);
+        Button loginButton = dialogView.findViewById(R.id.login_button);
+        Button cancelButton = dialogView.findViewById(R.id.cancel_button);
+
+        // 로그인 버튼 클릭 리스너
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 로그인 로직 구현
+                String username = usernameEditText.getText().toString();
+                String password = passwordEditText.getText().toString();
+                Toast.makeText(MainActivity.this, "Username: " + username + "\nPassword: " + password, Toast.LENGTH_SHORT).show();
+                dialog.dismiss();
+            }
+        });
+
+        // 취소 버튼 클릭 리스너
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
     }
+}
