a477b138eb60ce7ba6ad3c41c29f16d474787f59
subinsong01 <haely97@naver.com>
2024-06-12 17:40:13 +0900

    레이아웃 버튼 오류 발생 - 수정해야함

--- app/src/main/java/com/shushub/finalproject/activity_book.java
+++ app/src/main/java/com/shushub/finalproject/activity_book.java
@@ -10,7 +10,8 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class activity_book extends AppCompatActivity {
 
-    private boolean isListView = true;
+    private boolean isDetailView = false;
+    private boolean isSimpleView = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,31 +19,33 @@ public class activity_book extends AppCompatActivity {
         setContentView(R.layout.activity_book); // activity_book 레이아웃을 설정합니다.
 
         ImageView viewSwitchButton = findViewById(R.id.view_switch_button);
-        ImageView viewGridButton = findViewById(R.id.view_grid_button);
+        ImageView detailButton = findViewById(R.id.detail_btn);
 
         // 기본 프래그먼트 설정
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, new bookSimple())
+                .replace(R.id.fragment_container, new book_detail())
                 .commit();
+
+        detailButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                detailButton.setImageResource(R.drawable.list_type1); // 이미지를 변경
+                replaceFragment(new book_detail()); // book_detail 프래그먼트로 전환
+                isDetailView = true;
+            }
+        });
 
         viewSwitchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (isListView) {
-                    viewSwitchButton.setImageResource(R.drawable.list_type1); // 클릭 시 그리드 아이콘으로 변경
+                if (isDetailView) {
+                    viewSwitchButton.setImageResource(R.drawable.list_type22); // 클릭 시 리스트 아이콘으로 변경
                     replaceFragment(new book_detail());
                 } else {
-                    viewSwitchButton.setImageResource(R.drawable.list_type2); // 클릭 시 리스트 아이콘으로 변경
+                    viewSwitchButton.setImageResource(R.drawable.list_type2); // 클릭 시 그리드 아이콘으로 변경
                     replaceFragment(new bookSimple());
                 }
-                isListView = !isListView;
-            }
-        }); //수정해야함
-
-        viewGridButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                replaceFragment(new book_detail());
+                isDetailView = !isDetailView;
             }
         });
     }
