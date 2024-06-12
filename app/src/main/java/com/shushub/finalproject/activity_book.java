55fd46cd4203a600962bbf64320605981315d521
subinsong01 <haely97@naver.com>
2024-06-12 12:22:14 +0900

    레이아웃 버튼 오류 발생 (수정해야함)

--- app/src/main/java/com/shushub/finalproject/activity_book.java
+++ app/src/main/java/com/shushub/finalproject/activity_book.java
@@ -22,27 +22,27 @@ public class activity_book extends AppCompatActivity {
 
         // 기본 프래그먼트 설정
         getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, new book_detail())
+                .replace(R.id.fragment_container, new bookSimple())
                 .commit();
 
         viewSwitchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (isListView) {
-                    viewSwitchButton.setImageResource(R.drawable.list_type12); // 클릭 시 그리드 아이콘으로 변경
-                    replaceFragment(new bookSimple());
-                } else {
-                    viewSwitchButton.setImageResource(R.drawable.list_type22); // 클릭 시 리스트 아이콘으로 변경
+                    viewSwitchButton.setImageResource(R.drawable.list_type1); // 클릭 시 그리드 아이콘으로 변경
                     replaceFragment(new book_detail());
+                } else {
+                    viewSwitchButton.setImageResource(R.drawable.list_type2); // 클릭 시 리스트 아이콘으로 변경
+                    replaceFragment(new bookSimple());
                 }
                 isListView = !isListView;
             }
-        });
+        }); //수정해야함
 
         viewGridButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                replaceFragment(new bookSimple());
+                replaceFragment(new book_detail());
             }
         });
     }
